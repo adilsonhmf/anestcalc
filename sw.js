aeca096b780ea7eefe30a36fb7503187b67ee002
adilsonhmf <153024973+adilsonhmf@users.noreply.github.com>
2026-03-24 23:06:29 -0300

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'anestesia-pro-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Buscar do Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
