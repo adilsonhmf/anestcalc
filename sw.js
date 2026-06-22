4eadf83995ad5c1e4d107de7b0d2241e4c4e5193
adilsonhmf <153024973+adilsonhmf@users.noreply.github.com>
2026-06-22 16:58:24 -0300

    Downgrade CACHE_NAME to version 2.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'anestesia-pro-v6.4';
+const CACHE_NAME = 'anestesia-pro-v2.0 (downgrade)';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
